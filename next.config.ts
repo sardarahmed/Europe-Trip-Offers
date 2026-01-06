77a69421379d349b751cf465c5c32e22fb9cee0c
Sardar Ahmed <sardarahmednazar@gmail.com>
2026-01-06 17:13:23 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+  protocol: 'https',
+    hostname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-    ],
   },
 };
 
