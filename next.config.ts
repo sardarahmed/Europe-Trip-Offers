559053947e05e2ea9db7a54b897783c02e1eeb51
Sardar Ahmed <sardarahmednazar@gmail.com>
2026-01-06 17:14:56 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    remotePatterns: [
       {
-  protocol: 'https',
-    hostname: '**',
+        protocol: 'https',
+        hostname: '**',
       },
+    ],
   },
 };
 
