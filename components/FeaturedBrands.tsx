d21862b4169ff4b0ea28257241c2d6831ea19751
Sardar Ahmed <sardarahmednazar@gmail.com>
2026-03-06 21:20:47 +0100

    done

--- components/FeaturedBrands.tsx
+++ components/FeaturedBrands.tsx
@@ -22,7 +22,7 @@ export function FeaturedBrands({ stores }: FeaturedBrandsProps) {
                     </Link>
                 </div>
 
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
                     {stores.map((store, index) => (
                         <div
                             key={store.id}
